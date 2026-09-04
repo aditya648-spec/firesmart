8d545dce78b88b4d04889ef4cd1228899c40d781
Aditya Pillai <pillaiaditya648@gmail.com>
2026-09-04 22:22:47 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,18 +1,14 @@
-// firebase-config.js
-// Get these values from: Firebase Console > Project settings > General > Your apps > SDK setup
-// Project: smartfire-guardian
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCjFneNv4UqsfG8i46YXSeFuuEcLL3JE2A",
+  apiKey: "YOUR_API_KEY",
   authDomain: "smartfire-guardian.firebaseapp.com",
   databaseURL: "https://smartfire-guardian-default-rtdb.firebaseio.com",
   projectId: "smartfire-guardian",
   storageBucket: "smartfire-guardian.firebasestorage.app",
   messagingSenderId: "911423950287",
-  appId: "1:911423950287:web:5416e1f0ce6ef2150216ba",
+  appId: "1:911423950287:web:541e1f0ce6ef2150216ba",
   measurementId: "G-KTGH9K5HCJ"
 };
 
