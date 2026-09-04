34d4b34c4f255e072fed2d48d80953c05d4bce91
Aditya Pillai <pillaiaditya648@gmail.com>
2026-09-05 02:21:45 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,16 +1,41 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
+
+import {
+  getDatabase
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
+
 
 const firebaseConfig = {
+
   apiKey: "AIzaSyCjFneNv4UqsfG8i46YXSeFuuEcLL3JE2A",
-  authDomain: "smartfire-guardian.firebaseapp.com",
-  databaseURL: "https://smartfire-guardian-default-rtdb.firebaseio.com",
-  projectId: "smartfire-guardian",
-  storageBucket: "smartfire-guardian.firebasestorage.app",
-  messagingSenderId: "911423950287",
-  appId: "1:911423950287:web:541e1f0ce6ef2150216ba",
-  measurementId: "G-KTGH9K5HCJ"
+
+  authDomain:
+    "smartfire-guardian.firebaseapp.com",
+
+  databaseURL:
+    "https://smartfire-guardian-default-rtdb.firebaseio.com",
+
+  projectId:
+    "smartfire-guardian",
+
+  storageBucket:
+    "smartfire-guardian.firebasestorage.app",
+
+  messagingSenderId:
+    "911423950287",
+
+  appId:
+    "1:911423950287:web:5416e1f0ce6ef2150216ba",
+
+  measurementId:
+    "G-KTGH9K5HCJ"
+
 };
 
-const app = initializeApp(firebaseConfig);
-export const db = getDatabase(app);
+
+export const app =
+  initializeApp(firebaseConfig);
+
+
+export const db =
+  getDatabase(app);
